86100ec6061e810327c0cb6879462a82680a55e3
jtsalva <me@jtsalva.space>
2018-10-04 22:39:01 +0100

    serve on port 443

--- web/web.go
+++ web/web.go
@@ -8,13 +8,31 @@ import (
 	"github.com/jtsalva/estore/models"
 )
 
+const (
+	Domain string = "shop.jtsalva.space"
+
+	Certificate string = "/etc/letsencrypt/live/shop.jtsalva.space/cert.pem"
+	PrivateKey string = "/etc/letsencrypt/live/shop.jtsalva.space/privkey.pem"
+)
+
 func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", indexHandler)
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./content/static"))))
 
-	http.ListenAndServe(":80", r)
+	// Redirect port 80 traffic to 443
+	go func() {
+		if err := http.ListenAndServe(":80", http.HandlerFunc(redirectTLS)); err != nil {
+			log.Fatalf("ListenAndServe error: %v", err)
+		}
+	}()
+
+	http.ListenAndServeTLS(":443", Certificate, PrivateKey, r)
+}
+
+func redirectTLS(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://" + Domain + r.RequestURI, http.StatusMovedPermanently)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
